ab2bf8a1e81be8057b464f468ca01beb0c779889
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-14 13:35:21 +0300

    Deployment-2

--- config.py
+++ config.py
@@ -21,7 +21,9 @@ class Config:
 
 class ProdConfig(Config):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Vanilla@localhost/Blogs'
+
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Vanilla@localhost/Blogs'
