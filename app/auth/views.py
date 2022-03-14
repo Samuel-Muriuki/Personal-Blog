a138ec50b5e00268e8f2bffb76bab195d34b9cd6
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-14 10:00:58 +0300

    Creating the registration routing

--- app/auth/views.py
+++ app/auth/views.py
@@ -26,3 +26,16 @@ def logout():
     logout_user()
     flash('You have been successfully logged out')
     return redirect(url_for("main.index"))
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to the Bloging Website","email/welcome_user",user.email,user=user)
+        return redirect(url_for('auth.login'))
+        
+    return render_template('auth/register.html',registration_form = form)
