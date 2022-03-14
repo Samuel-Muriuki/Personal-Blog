c6787b44450f1cc7eb0a43e1ee32dbca847450e5
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-14 10:14:08 +0300

    Getting the random quotes

---
+++ app/main/views.py
@@ -0,0 +1,7 @@
+from flask import render_template,request,redirect,url_for,abort, flash, jsonify
+from . import main
+from .forms import BlogForm,UpdateProfile, CommentForm
+from ..models import User,Blog,Comment
+from flask_login import login_required, current_user
+from .. import db,photos
+from ..request import get_blogs
