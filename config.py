88bf61ae1989842f79808655570a3b584e65817c
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-14 09:47:41 +0300

    Creating the configurations file

---
+++ config.py
@@ -0,0 +1,42 @@
+import os
+
+class Config:
+    '''
+    Configuration class.
+    '''
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False    
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    # SUBJECT_PREFIX = 'Pitch'
+    SENDER_EMAIL = os.environ.get("MAIL_USERNAME")
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    DATABASE_URL='postgresql+psycopg2://postgres:Vanilla@localhost/Blogs'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    DEBUG = True
+
+class TestConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
+}
