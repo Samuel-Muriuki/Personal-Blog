1ac21787cde190b99e532f0a023a9ef9b8a5e81a
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-03-14 10:15:29 +0300

    Creating the landing page

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,17 @@ from ..models import User,Blog,Comment
 from flask_login import login_required, current_user
 from .. import db,photos
 from ..request import get_blogs
+
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to The Blogging Website'
+    blogs = get_blogs()
+
+    return render_template('index.html',title = title,blogs = blogs)
